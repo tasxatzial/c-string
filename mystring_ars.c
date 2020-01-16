cfd6c48ab4c1c04b6bc29fa6bb2ecf26720ef26e
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-16 07:42:08 +0200

    update doc

--- mystring_ars.c
+++ mystring_ars.c
@@ -1,14 +1,21 @@
 /* A string module: C string manipulation functions.
 
-Functions are declared in mystring.h
-
 Array based implementation */
 
 #include <stdio.h>
 #include "mystring.h"
 #include <assert.h>
 
-/* Description: See mystring.h */
+
+/* Calculates the length of the character array str, excluding
+the terminating null character.
+
+Checks: whether array is NULL at runtime.
+
+Parameters:
+str: character array. Must end with null char.
+
+Returns: length of str */
 size_t ms_length(char const str[]) {
     size_t str_length;
 
@@ -23,7 +30,17 @@ size_t ms_length(char const str[]) {
     return str_length;
 }
 
-/* Description: See mystring.h */
+
+/* Copies the character array src to the character array dest. Size of dest
+must be large enough to receive the copy or else the behavior is undefined. 
+
+Checks: whether both arrays are NULL at runtime.
+
+Parameters:
+src: source character array to copy from. Must end with null char.
+dest: destination array to copy to.
+
+Returns: pointer to destination array dest */
 char *ms_copy(char dest[], char const src[]) {
     size_t i;
 
@@ -43,7 +60,24 @@ char *ms_copy(char dest[], char const src[]) {
     return dest;
 }
 
-/* Description: See mystring.h */
+
+/* Copies at most num chars from the character array src to the character
+array dest. If there is no
+null character among the first num characters of src, dest will not be
+null-terminated. If length of src is less than num, the function writes
+additional null characters to dest to ensure that a total of num characters
+are written. Size of dest must be large enough to receive the copy or else
+the behavior is undefined.
+
+Checks: whether both arrays are NULL at runtime.
+
+Parameters:
+src: source character array to copy from. Must end with null char if its
+length <= num.
+dest: destination character array to copy to.
+num: number of characters.
+
+Returns: pointer to destination array dest */
 char *ms_ncopy(char dest[], char const src[], size_t num) {
     size_t i; 
 
@@ -66,7 +100,19 @@ char *ms_ncopy(char dest[], char const src[], size_t num) {
     return dest;
 }
 
-/* Description: See mystring.h */
+
+/* Appends the character array src to the character array dest.
+It overwrites the terminating null character of src and adds a terminating
+null character at the end of dest. Size of dest must be large enough to hold
+the contents of both src and dest or else the behavior is undefined.
+
+Checks: whether both arrays are NULL at runtime.
+
+Parameters:
+src: source character array to copy from. Must end with null char.
+dest: destination array to append to. Must end with null char.
+
+Returns: pointer to destination array dest */
 char *ms_concat(char dest[], char const src[]) {
     size_t i, dest_length;
 
@@ -87,7 +133,22 @@ char *ms_concat(char dest[], char const src[]) {
     return dest;
 }
 
-/* Description: See mystring.h */
+
+/* Appends at most num characters from the character array src
+string to the character array dest. It overwrites the terminating null
+character of src and adds a terminating null character at the end of dest.
+Size of dest must be large enough to hold the contents of both src and
+dest or else the behavior is undefined.
+
+Checks: whether both arrays are NULL at runtime.
+
+Parameters:
+src: source character array to copy from. Must end with null char if its
+length <= num.
+dest: destination character array to append to. Must end with null char.
+num: number of characters.
+
+Returns: pointer to destination array dest */
 char *ms_nconcat(char dest[], char const src[], size_t num) {
     size_t i, dest_length;
 
@@ -108,7 +169,20 @@ char *ms_nconcat(char dest[], char const src[], size_t num) {
     return dest;
 }
 
-/* Description: See mystring.h */
+
+/* Compares character arrays str1 and str2.
+
+Checks: whether both arrays are NULL at runtime.
+
+Parameters:
+str1: character array. Must end with null char.
+str2: character array. Must end with null char if
+      (length of str2 < length of str1)
+
+Returns:
+- An integer < 0 if str1 < str2
+- 0 if str1 = str2
+- An integer > 0 if str1 > str2 */
 int ms_compare(char const str1[], char const str2[]) {
     size_t i;
 
@@ -127,7 +201,21 @@ int ms_compare(char const str1[], char const str2[]) {
     return str1[i] - str2[i];
 }
 
-/* Description: See mystring.h */
+
+/* Compares the first num characters of character arrays str1 and str2.
+
+Checks: whether both arrays are NULL at runtime.
+
+Parameters:
+str1: character array. Must end with null char if
+      (length of str1 <= num)
+str2: character array. Must end with null char if
+      (length of str2 < length of str1 && length of str2 <= num)
+
+Returns:
+- An integer < 0 if str1 < str2
+- 0 if str1 = str2
+- An integer > 0 if str1 > str2 */
 int ms_ncompare(char const str1[], char const str2[], size_t num) {
     size_t i;
 
@@ -153,7 +241,18 @@ int ms_ncompare(char const str1[], char const str2[], size_t num) {
     return str1[i] - str2[i];
 }
 
-/* Description: See mystring.h */
+
+/* Finds the first occurence of the character array needle
+in the character array haystack. The terminating null characters are not
+compared.
+
+Checks: whether both arrays are NULL at runtime.
+
+Parameters:
+haystack: character array. Must end with null char.
+needle: character array. Must end with null char.
+
+Returns: if needle is found a pointer to it, else NULL */
 char *ms_search(char const haystack[], char const needle[]) {
     size_t i, j, haystack_length, needle_length;
 
--- mystring_ptrs.c
+++ mystring_ptrs.c
@@ -1,14 +1,21 @@
 /* A string module: C string manipulation functions.
 
-Functions are declared in mystring.h
-
 Pointer based implementation */
 
 #include <stdio.h>
 #include "mystring.h"
 #include <assert.h>
 
-/* Description: See mystring.h */
+
+/* Calculates the length of the character array str, excluding
+the terminating null character.
+
+Checks: whether array is NULL at runtime.
+
+Parameters:
+str: character array. Must end with null char.
+
+Returns: length of str */
 size_t ms_length(char const *str) {
     char const *str_ptr;
     
@@ -23,7 +30,17 @@ size_t ms_length(char const *str) {
     return str_ptr - str;
 }
 
-/* Description: See mystring.h */
+
+/* Copies the character array src to the character array dest. Size of dest
+must be large enough to receive the copy or else the behavior is undefined. 
+
+Checks: whether both arrays are NULL at runtime.
+
+Parameters:
+src: source character array to copy from. Must end with null char.
+dest: destination array to copy to.
+
+Returns: pointer to destination array dest */
 char *ms_copy(char *dest, char const *src) {
     char const *src_ptr;
     char *dest_ptr;
@@ -44,7 +61,24 @@ char *ms_copy(char *dest, char const *src) {
     return dest;
 }
 
-/* Description: See mystring.h */
+
+/* Copies at most num chars from the character array src to the character
+array dest. If there is no
+null character among the first num characters of src, dest will not be
+null-terminated. If length of src is less than num, the function writes
+additional null characters to dest to ensure that a total of num characters
+are written. Size of dest must be large enough to receive the copy or else
+the behavior is undefined.
+
+Checks: whether both arrays are NULL at runtime.
+
+Parameters:
+src: source character array to copy from. Must end with null char if its
+length <= num.
+dest: destination character array to copy to.
+num: number of characters.
+
+Returns: pointer to destination array dest */
 char *ms_ncopy(char *dest, char const *src, size_t num) {
     char const *src_ptr;
     char *dest_ptr;
@@ -69,7 +103,19 @@ char *ms_ncopy(char *dest, char const *src, size_t num) {
     return dest;
 }
 
-/* Description: See mystring.h */
+
+/* Appends the character array src to the character array dest.
+It overwrites the terminating null character of src and adds a terminating
+null character at the end of dest. Size of dest must be large enough to hold
+the contents of both src and dest or else the behavior is undefined.
+
+Checks: whether both arrays are NULL at runtime.
+
+Parameters:
+src: source character array to copy from. Must end with null char.
+dest: destination array to append to. Must end with null char.
+
+Returns: pointer to destination array dest */
 char *ms_concat(char *dest, char const *src) {
     char const *src_ptr;
     char *dest_ptr;
@@ -90,7 +136,22 @@ char *ms_concat(char *dest, char const *src) {
     return dest;
 }
 
-/* Description: See mystring.h */
+
+/* Appends at most num characters from the character array src
+string to the character array dest. It overwrites the terminating null
+character of src and adds a terminating null character at the end of dest.
+Size of dest must be large enough to hold the contents of both src and
+dest or else the behavior is undefined.
+
+Checks: whether both arrays are NULL at runtime.
+
+Parameters:
+src: source character array to copy from. Must end with null char if its
+length <= num.
+dest: destination character array to append to. Must end with null char.
+num: number of characters.
+
+Returns: pointer to destination array dest */
 char *ms_nconcat(char *dest, char const *src, size_t num) {
     char const *src_ptr;
     char *dest_ptr;
@@ -111,7 +172,20 @@ char *ms_nconcat(char *dest, char const *src, size_t num) {
     return dest;
 }
 
-/* Description: See mystring.h */
+
+/* Compares character arrays str1 and str2.
+
+Checks: whether both arrays are NULL at runtime.
+
+Parameters:
+str1: character array. Must end with null char.
+str2: character array. Must end with null char if
+      (length of str2 < length of str1)
+
+Returns:
+- An integer < 0 if str1 < str2
+- 0 if str1 = str2
+- An integer > 0 if str1 > str2 */
 int ms_compare(char const *str1, char const *str2) {
     char const *ptr1, *ptr2;
     
@@ -133,7 +207,20 @@ int ms_compare(char const *str1, char const *str2) {
 }
 
 
-/* Description: See mystring.h */
+/* Compares the first num characters of character arrays str1 and str2.
+
+Checks: whether both arrays are NULL at runtime.
+
+Parameters:
+str1: character array. Must end with null char if
+      (length of str1 <= num)
+str2: character array. Must end with null char if
+      (length of str2 < length of str1 && length of str2 <= num)
+
+Returns:
+- An integer < 0 if str1 < str2
+- 0 if str1 = str2
+- An integer > 0 if str1 > str2 */
 int ms_ncompare(char const *str1, char const *str2, size_t num)
 {
     char const *ptr1, *ptr2;
@@ -162,7 +249,18 @@ int ms_ncompare(char const *str1, char const *str2, size_t num)
     return *ptr1 - *ptr2;
 }
 
-/* Description: See mystring.h */
+
+/* Finds the first occurence of the character array needle
+in the character array haystack. The terminating null characters are not
+compared.
+
+Checks: whether both arrays are NULL at runtime.
+
+Parameters:
+haystack: character array. Must end with null char.
+needle: character array. Must end with null char.
+
+Returns: if needle is found a pointer to it, else NULL */
 char *ms_search(char const *haystack, char const *needle) {
     size_t haystack_length, needle_length;
     char const *haystack_ptr;
